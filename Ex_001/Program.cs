ede4fd5aef8f88f1e9b2d08b167367404b1c2fc3
Mikhail Malafeew <malafeew2016@gmail.com>
2022-08-13 00:11:38 +0300

    NewExample_1001

--- Ex_001/Program.cs
+++ Ex_001/Program.cs
@@ -1,179 +1,177 @@
-﻿// 5. Конвертер валют. У пользователя есть баланс в каждой из представленных валют.
-// С помощью команд он может попросить сконвертировать одну валюту в другую.
-// Курс конвертации просто описать в программе. Программа заканчивает свою работу в тот момент,
-// когда решит пользователь.
-int billDollar = 5; //5 долларова
-int billEuro = 5; // 5 евро
-int biilRubles = 350; // 350 рублей
-// int billYuan = 40;// 40 юаней
-// int billTenge = 2800;//2800 тенге
+﻿//************BeginnigDictionaries************//
+var rate = new Dictionary<string,double>()
+{
+    ["rub"] = 1   ,
+    ["usd"] = 60,
+    ["eur"] = 62,
+    ["chf"] = 65,
+    ["cny"] = 9
+};
+
+var currenciesAvailable = new Dictionary<string,string>()
+{
+    ["rub"] = "Российский рубль",
+    ["usd"] = "Американский доллар",
+    ["eur"] = "Американское евро",
+    ["chf"] = "Швейцарский франк",
+    ["cny"] = "Китайский юань"
+};
+var balance = new Dictionary<string,double>()
+{
+    ["rub"] = 6000,
+    ["usd"] = 100,
+    ["eur"] = 100,
+    ["chf"] = 95,
+    ["cny"] = 700
+};
+//************BeginnigMethods************//
+void OutputCourse (Dictionary<string,double> dic)
+{
+    foreach(var course in dic)
+    {
+        Console.WriteLine($"{course.Key} = {course.Value}");
+    }
+}
+
+void OutputCurrency(Dictionary<string,string> dic)
+{
+    foreach(var currency in dic)
+    {
+        Console.WriteLine($"{currency.Key} = {currency.Value}");
+    }
+}
+void OutputBalance(Dictionary<string,double> dictionar)
+{
+    foreach(var balance in dictionar)
+    {
+        Console.WriteLine($"{balance.Key} = {balance.Value}");
+    }
+}
+void ConvertCurrency (Dictionary<string,double> balance, Dictionary<string,double> rate,string login, int password)
+{
+    Console.WriteLine("Для доступа к конвертированию введите логин и пароль!") ;
+    Console.Write("Введите логин: ");
+    string checklogin = Console.ReadLine();
+    Console.Write("Введите пароль: ");
+    int checkpassword = Convert.ToInt32(Console.ReadLine());
+    
+    if(checklogin == login && checkpassword == password)
+    {
+        Console.WriteLine("Вход разрешен.");
+        bool statusConversions = true;
+        while(statusConversions)
+        {
+            Console.Write("Хотите конвертировать валюту(Если хотите, напишете - 'yes', если нет, то напишите - 'no' ): ");
+            if(Console.ReadLine().ToLower() == "yes")
+            {
+                Console.Write("Введите валюту, которую хотите конвертировать: ");
+                string firstCurrency = Console.ReadLine();
+                Console.Write("Введите валюту, в которую хотите конвертировать " + firstCurrency + ": ");
+                string secondCurrency = Console.ReadLine();
+                Console.Write($"Введите количество {firstCurrency}, которое вы хотите конвертировать в {secondCurrency}: ");
+                double numberConversions = Convert.ToDouble(Console.ReadLine());
+                if(firstCurrency.ToLower() != "rub" && secondCurrency.ToLower() !="rub")
+                {  
+                    if(numberConversions <= balance[firstCurrency])
+                    {   
+                        double firstCount = numberConversions * rate[firstCurrency];  //balance[firstCurrency] * rate[firstCurrency];
+                        double secondCount = Math.Round(firstCount / rate[secondCurrency],2);
+                        double manipulatroOne =  balance[firstCurrency] - numberConversions;
+                        Console.WriteLine($"после конвертации {firstCurrency} в {secondCurrency} на вашем счете будет {balance[secondCurrency] + secondCount} {secondCurrency}  и  {manipulatroOne} {firstCurrency};");
+                        Console.Write("Конвертируем?:('yes' or 'not') : ");
+                        if(Console.ReadLine().ToLower() == "yes")
+                        {
+                            balance[secondCurrency] += secondCount;
+                            balance[firstCurrency] -= numberConversions;
+                            Console.WriteLine("Конвертация прошла очень успешно!)");
+                        }
+                    }   
+                }
+                else if(firstCurrency.ToLower() == "rub")
+                {       
+                    if(numberConversions <= balance[firstCurrency])
+                        {   
+                            double firstCount =  Math.Round(numberConversions/ rate[secondCurrency] ,  2)  ;
+                            double manipulatroOne =  balance[firstCurrency] - numberConversions;
+                            Console.WriteLine($"после конвертации {firstCurrency} в {secondCurrency} на вашем счете будет {balance[secondCurrency] + firstCount} {secondCurrency}  и  {manipulatroOne} {firstCurrency};");
+                            Console.Write("Конвертируем?:('yes' or 'not') : ");
+                            if(Console.ReadLine().ToLower() == "yes")
+                            {
+                                balance[secondCurrency] += firstCount;
+                                balance[firstCurrency] -= numberConversions;
+                                Console.WriteLine("Конвертация прошла очень успешно!)");
+                            }
+                        }  
+                }
+                else if(secondCurrency.ToLower() == "rub")
+                {       
+                    if(numberConversions <= balance[firstCurrency])
+                        {   
+                            double firstCount =  Math.Round( numberConversions * rate[firstCurrency],2) ;
+                            double manipulatorOne =  balance[firstCurrency] - numberConversions;
+                            Console.WriteLine($"после конвертации {firstCurrency} в {secondCurrency} на вашем счете будет {balance[secondCurrency] + firstCount} {secondCurrency}  и  {manipulatorOne} {firstCurrency};");
+                            Console.Write("Конвертируем?:('yes' or 'not') : ");
+                            if(Console.ReadLine().ToLower() == "yes")
+                            {
+                                balance[secondCurrency] += firstCount;
+                                balance[firstCurrency] -= numberConversions;
+                                Console.WriteLine("Конвертация прошла очень успешно!)");
+                            }
+                        }  
+                } 
+            }
+            else
+            {
+                statusConversions = false;
+            }
+        }
+    }
+    else
+    {
+        Console.WriteLine("В доступе отказано!(");
+    }   
+}
+//************BeginningCode**********//
 bool isWork = true;
-int dollarRubles = 60;
-int dollarEuro = 1;
-int euroRub = 60;
-// int dollarYuan = 7;
-// int dollarTenge = 480;
-
-Console.WriteLine("\nВас приветствует самый крутой сервис по конвертированию валют.");
-Console.WriteLine("В честь нашего открытия, у вас после регистрации в кошельке уже будет валюта различного номинала.");
-Console.Write("Хотите зарегестрироваться?(Введите 'да' 'нет'): ");
+string workDecision ="";
+Console.WriteLine("Приветствуем вас в нашем сервисе- конвертере. В честь нашего открытия, после регистрации, на вашем счету будет валюта разного номинала\n");
+Console.Write("Хотите зарегестрироваться?(Введите 'yes' or 'no'): ");
 string registrationStatus = Console.ReadLine();
-
-if(registrationStatus.ToLower() == "да")
+if(registrationStatus.ToLower() == "yes")
 {
     Console.Write("Введите логин: ");
     string login = Console.ReadLine();
     Console.Write("Введите пароль: ");
     int password = Convert.ToInt32(Console.ReadLine());
-
     Console.WriteLine("Регистрация прошла успешно!)Приятного пользования нашим сервисом\n");
-
     while(isWork)
+    {
+        Console.Write("Введите, что вы хотите сделать(Чтобы получить инструкцию, напишите - 'help') : ");
+        workDecision = Console.ReadLine();
+        switch(workDecision.ToLower())
         {
-            Console.Write("Напишите то,что выхотите сделать(Чтобы получить инструкцию напишите - 'help'): ");
-            string decision = Console.ReadLine();
-            switch(decision.ToLower())
-            {
-                case "help" :
-                    Console.WriteLine("Все возможности нашего конвертера:\n");
-                    Console.WriteLine("Balance - вывести имеющиеся валюты на вашем кошельке");
-                    Console.WriteLine("ChangePassword - изменить пароль");
-                    Console.WriteLine("ExchangeRate - узнать курс валют и конвертировать деньги");
-                    Console.WriteLine("Exit - выйти из этой программы. ");
-                    break;
-                case "exit":
-                    isWork = false;
-                    break;
-                case "changepassword":
-                    Console.Write("Чтобы установить новый пароль - введите старый пароль: ");
-                    int oldPassword = Convert.ToInt32(Console.ReadLine());
-                    if(oldPassword == password)
-                    {
-                        Console.WriteLine("Старый пароль введен успешно!");
-                        Console.Write("Введите новый пароль: ");
-                        password = Convert.ToInt32(Console.ReadLine());//Можно было бы еще через одну переменную сделать,но та практичнее и быстрей.                     
-                        Console.WriteLine("Новый пароль успешно установлен.");
-                    }
-                    break;
-                case  "balance": 
-                    Console.WriteLine($"У вас имеется вот столько рублей : {biilRubles}");
-                    Console.WriteLine($"У вас имеется вот столько долларов : {billDollar}");
-                    Console.WriteLine($"У вас имеется вот столько евро : {billEuro}");                   
-                    // Console.WriteLine($"У вас имеется вот столько юаней : {billYuan}");
-                    // Console.WriteLine($"У вас имеется вот столько тенге : {billTenge}\n");
-                    break;
-                
-                
-                case "exchangerate" :
-                    Console.WriteLine("Доступные валюты для конвертации: ruble,dollar,euro");  //yuan,tenge
-                    Console.WriteLine("1 Dollar = 60 Rubles\n1 Dollar = 1 Euro\n1 Euro = 60 Rubles ");
-                    Console.Write("Хотите конвертировать валюту?: ");
-                    string conversionSolution = Console.ReadLine();
-                    if(conversionSolution.ToLower() == "да")
-                    {
-                        Console.Write("Введите пароль от аккаунта: ");
-                        int authorizationPassword = Convert.ToInt32(Console.ReadLine());
-                        if(authorizationPassword == password )
-                        {
-                            Console.Write("Введите валюту, которую хотите конвертировать: ");
-                            string currencyOne = Console.ReadLine();
-                            Console.Write("Введите валюту, в которую хотите конвертировать: ") ;
-                            string currencyTwo = Console.ReadLine();
-
-                            if(currencyOne.ToLower() == "dollar" && currencyTwo.ToLower() == "ruble")
-                            {
-                                Console.WriteLine($"После конвертации вы получите вот столько рублей: {billDollar * dollarRubles}");
-                                Console.Write("Если вы согласны, напишите 'да', если нет, то напишите 'нет': ");
-                                string conversionOpinion = Console.ReadLine();
-                                if(conversionOpinion.ToLower() == "да" )
-                                    {
-                                        biilRubles +=  billDollar * dollarRubles;
-                                        billDollar -= billDollar;
-                                        Console.WriteLine($"Конвертация прошла успешно, на вашем счету теперь вот столько рублей - {biilRubles} и вот столько долларов - {billDollar}");
-                                    }
-                            }
-                            else if(currencyOne.ToLower() == "ruble" && currencyTwo.ToLower() == "dollar")
-                            {
-                                Console.WriteLine($"После конвертации вы получите вот столько долларов: {biilRubles / dollarRubles}");
-                                Console.Write("Если вы согласны, напишите 'да', если нет, то напишите 'нет': ");
-                                string conversionOpinion = Console.ReadLine();
-                                if(conversionOpinion.ToLower() == "да" )
-                                    {
-                                        billDollar +=  biilRubles /  dollarRubles   ;
-                                        biilRubles = biilRubles  % dollarRubles ;
-                                        Console.WriteLine($"Конвертация прошла успешно, на вашем счету теперь вот столько долларов - {billDollar} и вот столько рублей - {biilRubles}");
-                                    }
-                            }
-                            else if(currencyOne.ToLower() == "euro" && currencyTwo.ToLower() == "dollar")
-                            {
-                                Console.WriteLine($"После конвертации вы получите вот столько долларов: {billDollar * dollarEuro}");
-                                Console.Write("Если вы согласны, напишите 'да', если нет, то напишите 'нет': ");
-                                string conversionOpinion = Console.ReadLine();
-                                if(conversionOpinion.ToLower() == "да" )
-                                    {
-                                        billDollar +=  billDollar * dollarEuro;
-                                        billEuro -= billEuro;
-                                        Console.WriteLine($"Конвертация прошла успешно, на вашем счету теперь вот столько долларов - {billDollar} и вот столько евро - {billEuro}");
-                                    }
-                            }
-                            else if(currencyOne.ToLower() == "dollar" && currencyTwo.ToLower() == "euro")
-                            {
-                                Console.WriteLine($"После конвертации вы получите вот столько евро: {billDollar}");
-                                Console.Write("Если вы согласны, напишите 'да', если нет, то напишите 'нет': ");
-                                string conversionOpinion = Console.ReadLine();
-                                if(conversionOpinion.ToLower() == "да" )
-                                    {
-                                        billEuro +=  billDollar;
-                                        billDollar -= billDollar;
-                                        Console.WriteLine($"Конвертация прошла успешно, на вашем счету теперь вот столько евро - {billEuro} и вот столько долларов - {billDollar}");
-                                    }                           
-                            }
-                            else if(currencyOne.ToLower() == "ruble" && currencyTwo.ToLower() == "euro")
-                            {
-                                Console.WriteLine($"После конвертации вы получите вот столько евро: {biilRubles / euroRub}");
-                                Console.Write("Если вы согласны, напишите 'да', если нет, то напишите 'нет': ");
-                                string conversionOpinion = Console.ReadLine();
-                                if(conversionOpinion.ToLower() == "да" )
-                                    {
-                                        billEuro =  biilRubles / euroRub;
-                                        biilRubles = biilRubles % euroRub;
-                                        Console.WriteLine($"Конвертация прошла успешно, на вашем счету теперь вот столько евро - {billEuro} и вот столько рублей - {biilRubles}");
-                                    }                           
-                            }
-                            else if(currencyOne.ToLower() == "euro" && currencyTwo.ToLower() == "ruble")
-                            {
-                                Console.WriteLine($"После конвертации вы получите вот столько рублей: {billEuro * euroRub}");
-                                Console.Write("Если вы согласны, напишите 'да', если нет, то напишите 'нет': ");
-                                string conversionOpinion = Console.ReadLine();
-                                if(conversionOpinion.ToLower() == "да" )
-                                    {
-                                        biilRubles =  billEuro *euroRub;
-                                        billEuro -= billEuro;
-                                        Console.WriteLine($"Конвертация прошла успешно, на вашем счету теперь вот столько рублей - {biilRubles} и вот столько евро - {billEuro}");
-                                    }                           
-                            }
-                            
-                            break;
-                        }
-                        else 
-                        {
-                            break;
-                        }
-                    }
-                    else
-                    {
-                        break;
-                    }
-                    break;
-            }
+            case "help":
+                Console.WriteLine("Exchangerate - узнать обменный курс.\nCurrenciesExchange - узнать наименования валют.\nConvertCurrency - конвертировать свою валюту.\nBalance - узнать свой баланс\nExit -  выйти  из программы");
+                break;
+            case "exit" :
+                isWork = false;
+                break;
+            case "exchangerate":
+                OutputCourse(rate);
+                break;
+            case "currenciesexchange":
+                OutputCurrency(currenciesAvailable);
+                break;
+            case "convertcurrency" :
+                ConvertCurrency(balance, rate, login, password);
+                break;
+            case "balance" :
+                OutputBalance(balance);
+                break;
         }
+    }
 }
-else{
-    Console.WriteLine("Досвидания, до скорых встреч.!)");
+else
+{
+    Console.WriteLine("Досвидания!");
 }
-
-
-
-
-
-
